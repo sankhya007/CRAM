de2fd56307adc2b075e81d0bed8afc858a46dc32
sankhya007 <sankhyapriyo2004@gmail.com>
2026-07-12 22:53:18 +0530

    lzw_algo full

--- LZW_algo.py
+++ LZW_algo.py
@@ -1,9 +1,11 @@
+import json
+
 def compress(uncompressed):
     """Compress a string to a list of output symbols."""
 
     # Build the dictionary.
     dict_size = 256
-    dictionary = dict((chr(i), chr(i)) for i in range(dict_size))
+    dictionary = {chr(i): i for i in range(dict_size)}
 
     w = ""
     result = []
@@ -28,40 +30,52 @@ def decompress(compressed):
 
     # Build the dictionary.
     dict_size = 256
-    dictionary = dict((chr(i), chr(i)) for i in range(dict_size))
+    dictionary = {i: chr(i) for i in range(dict_size)}
 
-    w = result = compressed.pop(0)
-    for k in compressed:
+    w = dictionary[compressed[0]]
+    result = [w]
+    
+    for k in compressed[1:]:
         if k in dictionary:
             entry = dictionary[k]
         elif k == dict_size:
             entry = w + w[0]
         else:
             raise ValueError('Bad compressed k: %s' % k)
-        result += entry
 
         # Add w+entry[0] to the dictionary.
+        result.append(entry)
         dictionary[dict_size] = w + entry[0]
         dict_size += 1
-
         w = entry
-    return result
+        
+    return "".join(result)
 
-# encode block 
-try:
-    print("file found, processing...")
-    with open("example.txt", "r") as f: # read the file and store
-        encoded_text = (f.read())
-    with open("LZW_compressed.bin", "w") as f: # compress and then store in bin
-        compressed = compress(encoded_text)
-        print(compressed, file=f)
-except FileNotFoundError: 
-    print("file was not fount  exists in the root folder...")
-    
-print("File was compressed successfully and saved to output path...")
+# # encode block 
+# try:
+#     print("file found, processing...")
+#     with open("example.txt", "r") as f: # read the file and store
+#         encoded_text = f.read()
+#     compressed = compress(encoded_text)
+#     with open("LZW_compressed.bin", "w") as f: # compress and then store in bin
+#         json.dump(compressed, f)
+#     print("File was compressed successfully...")
+# except FileNotFoundError: 
+#     print("file was not found in the root folder...")  
+
 
 # decode block
-
+try: 
+    print("file found, starting to process...")
+    with open("lzw_compressed.bin", "r") as f: 
+        decode_text = json.load(f)
+    decompressed = decompress(decode_text)
+    with open("lzw_decoded.txt", "w") as f: 
+        f.write(decompressed)
+    print("File decompression was done successfully...")
+except FileNotFoundError:
+    print("file was not found in the root folder...") 
+        
 
 print()
     
